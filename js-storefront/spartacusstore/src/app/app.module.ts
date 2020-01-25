a42550d605c40823997a773eb863ae43756e7e94
Thomas-Sparrevohn <57354823+Thomas-Sparrevohn@users.noreply.github.com>
2020-01-25 10:01:34 +0000

    Magic does not work in app.module.ts
    Means that build control has to be through Angular environments.ts files or somehow else

--- js-storefront/spartacusstore/src/app/app.module.ts
+++ js-storefront/spartacusstore/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     B2cStorefrontModule.withConfig({
       backend: {
-        occ: {baseUrl: 'OCC_BACKEND_BASE_URL_VALUE',
+        occ: {baseUrl: 'https://api.cb6zx0i-cultbeaut1-d1-public.model-t.cc.commerce.ondemand.com',
           prefix: '/rest/v2/'
         }
       },
